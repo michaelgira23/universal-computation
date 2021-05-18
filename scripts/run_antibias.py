4357e8c820c5d2779fb0b26a45daa1f5c0dc12b4
ruisu516 <56084678+ruisu516@users.noreply.github.com>
2021-05-18 17:13:50 +0800

    Update run_antibias.py

--- scripts/run_antibias.py
+++ scripts/run_antibias.py
@@ -1,31 +1,31 @@
+import sys
+sys.path.append('./')
 from universal_computation.experiment_antibias import run_experiment
 
 if __name__ == '__main__':
 
-    experiment_name = 'fpt'
+    experiment_name = 'fpt_antibias'
 
     experiment_params = dict(
         task='antibias',
-        n=1000,  # ignored if not a bit task
-        num_patterns=5,  # ignored if not a bit task
-        patch_size=50,
         model_name='gpt2',
+        input_max_dim = 50,
         pretrained=True,
         freeze_trans=True,  # if False, we don't check arguments other than in and out
-        freeze_in=False,
+        freeze_linear=False,
         freeze_pos=False,
         freeze_ln=False,
         freeze_attn=True,
         freeze_ff=True,
         freeze_out=False,
-        in_layer_sizes=
-        None,  # not in paper, but can specify layer sizes for an MLP,
-        out_layer_sizes=
-        None,  # ex. [32, 32] creates a 2-layer MLP with dimension 32
+        linear_layer_sizes= None,  # not in paper, but can specify layer sizes for an MLP,
+        out_layer_sizes=None,  # ex. [32, 32] creates a 2-layer MLP with dimension 32
         learning_rate=1e-3,
         batch_size=2,
         dropout=0.1,
         orth_gain=1.41,
+        position_ids = None,
+        return_last_only = True,
     )
 
     run_experiment(experiment_name, experiment_params)
