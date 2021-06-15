b3e07ec0c7b5c1a4a8013e3dd47a0bbb6b493542
ruisu516 <56084678+ruisu516@users.noreply.github.com>
2021-06-15 15:43:03 +0800

    load label from dataset

--- universal_computation/datasets/anti_bias.py
+++ universal_computation/datasets/anti_bias.py
@@ -9,10 +9,12 @@ import tensorflow_datasets
 AUTOTUNE = tf.data.experimental.AUTOTUNE
 
 class AntiBiasDataset(Dataset):
-    def __init__(self, batch_size, data_dir = "../universal-computation/unprejudiced_dataset/",model_name = "gpt2",input_max_dim = 50,*args, **kwargs):
+    def __init__(self, device,vocab_size,batch_size, data_dir = "../universal-computation/unprejudiced_dataset/",model_name = "gpt2",input_max_dim = 50,*args, **kwargs):
         super().__init__(*args, **kwargs)
 
         self.batch_size = batch_size  # we fix it so we can use dataloader
+        self.device = device
+        self.vocab_size = vocab_size
 
         from transformers import GPT2Tokenizer
         self.tokenizer = GPT2Tokenizer.from_pretrained(model_name)
@@ -23,8 +25,13 @@ class AntiBiasDataset(Dataset):
         tokenized_train = self.tokenizer(file_train)['input_ids']
         tokenized_test = self.tokenizer(file_test)['input_ids']
 
-        padded_tokenized_train = [[0]*max(0,input_max_dim-len(tokenized_train[i])) + tokenized_train[i][:min(input_max_dim,len(tokenized_train[i]))] for i in range(len(tokenized_train))]
-        padded_tokenized_test = [[0]*max(0,input_max_dim-len(tokenized_test[i])) + tokenized_test[i][:min(input_max_dim,len(tokenized_test[i]))] for i in range(len(tokenized_test))]
+        padded_tokenized_train = [[tokenized_train[i][min(input_max_dim,len(tokenized_train[i])-1)]] + tokenized_train[i][:min(input_max_dim,len(tokenized_train[i])-1)] + [0]*max(0,input_max_dim+1-len(tokenized_train[i])) for i in range(len(tokenized_train))]
+        padded_tokenized_test = [[tokenized_test[i][min(input_max_dim,len(tokenized_test[i])-1)]] + tokenized_test[i][:min(input_max_dim,len(tokenized_test[i])-1)] + [0]*max(0,input_max_dim+1-len(tokenized_test[i])) for i in range(len(tokenized_test))]
+
+        # tokenized_train_y = [0] * vocab_size
+        # tokenized_train_y[tokenized_train[:-1]] = 1
+        # tokenized_test_y = [0] * vocab_size
+        # tokenized_test_y[tokenized_test[:-1]] = 1
 
         #convert list to tensorflow Dataset
         self.train_dataset = tf.data.Dataset.from_tensor_slices(padded_tokenized_train)
@@ -53,14 +60,19 @@ class AntiBiasDataset(Dataset):
             if batch is None:
                 self.test_enum = iter(tensorflow_datasets.as_numpy(self.d_test))
                 batch = next(self.test_enum)
-
-        x = torch.from_numpy(batch).long()
+        
+        x = torch.from_numpy(batch[:,1:]).long()
+        y_indices = batch[:,1]
+        y = np.zeros((y_indices.size, self.vocab_size))
+        y[np.arange(y_indices.size),y_indices] = 1
+        y = torch.from_numpy(y).long()
         
         x = x.to(device=self.device)
+        y = y.to(device=self.device)
 
         self._ind += 1
 
-        return x
+        return x,y
 
 
         
